ea5aa5595c30951ed54d113b9c841e3e0c3efa44
adozoo <adozoo.c@gmail.com>
2023-02-26 16:13:11 +0900

    dev

--- src/pages/ButtonPage.vue
+++ src/pages/ButtonPage.vue
@@ -5,7 +5,7 @@
         <ion-buttons slot="start">
           <ion-back-button></ion-back-button>
         </ion-buttons>
-        <ion-title>button按钮组件演示</ion-title>
+        <ion-title>button按钮组件演示andre</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content>
